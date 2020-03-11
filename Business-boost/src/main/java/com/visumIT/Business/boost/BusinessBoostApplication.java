b5d194df2a9d08c9cc82077289ee406a3288976e
karlgama <kaiquesilva49@hotmail.com>
2020-03-11 17:04:48 -0300

    criando cadastro da empresa model

---
+++ Business-boost/src/main/java/com/visumIT/Business/boost/BusinessBoostApplication.java
@@ -0,0 +1,13 @@
+package com.visumIT.Business.boost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessBoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessBoostApplication.class, args);
+	}
+
+}
---
+++ Business-boost/src/test/java/com/visumIT/Business/boost/BusinessBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visumIT.Business.boost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessBoostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

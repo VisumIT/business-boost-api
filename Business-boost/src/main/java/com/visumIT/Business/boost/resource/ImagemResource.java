6109b9e3e34ec608dc12abfbe82f84c63d3e8149
Karl Gama <kaiquesilva49@hotmail.com>
2020-05-28 13:33:44 -0300

    Resolvendo bug das imagens

--- Business-boost/src/main/java/com/visumIT/Business/boost/resource/ImagemResource.java
+++ Business-boost/src/main/java/com/visumIT/Business/boost/resource/ImagemResource.java
@@ -4,7 +4,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,13 +22,32 @@ public class ImagemResource {
 	
 	private String uploadedFolder = "/home/karl/TCC/Desenvolvimento/tmp/imagens/";
 
+	private String[] types = {"image/jpg","image/png"};
+
+	
+	
+	public Boolean contain(String e, String[] types) {
+		for(int i = 0; i<types.length;++i) {
+			if(e==types[i]) {
+				return true;
+			}
+		}return false;
+		
+	}
 	
 	@PostMapping("/upload")
-	public String singleFileUpload(@RequestBody MultipartFile file, RedirectAttributes redirectAttributes) {
+	public ResponseEntity<?> singleFileUpload(@RequestBody MultipartFile file, RedirectAttributes redirectAttributes) {
+	
+
+		String imageType = file.getContentType().toString();
+		if(!contain(imageType,types)){
+			return ResponseEntity.badRequest().body(new JSONObject().put("message", "tipo errado").toString());
+		}
 		
-		if(file.isEmpty()) {
-			redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
-			return "redirect:uploadStatus";
+		if(file.isEmpty() ){
+			redirectAttributes.addFlashAttribute("message", "Please select a image to upload");
+			return ResponseEntity.badRequest().body(imageType);
+			//return ResponseEntity.badRequest().body(new JSONObject().put("message", "Please select a image to upload").toString());
 		}
 		try {
 			byte[] bytes = file.getBytes();
@@ -36,7 +60,7 @@ public class ImagemResource {
 			e.printStackTrace();
 		}
 		
-		return "redirect:/uploadStatus";
+		return ResponseEntity.ok().build();
 	}
 	
 	@GetMapping("/uploadStatus")

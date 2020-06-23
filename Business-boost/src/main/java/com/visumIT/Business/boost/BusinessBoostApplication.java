9146ca5dc370e46df7538f66ffaca7cee406f304
“WesleyMeneghini” <wesley.meneghini@outlook.com>
2020-06-22 23:52:40 -0300

    colocando arquivo de configuração do firebase

--- Business-boost/src/main/java/com/visumIT/Business/boost/BusinessBoostApplication.java
+++ Business-boost/src/main/java/com/visumIT/Business/boost/BusinessBoostApplication.java
@@ -2,15 +2,12 @@ package com.visumIT.Business.boost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.visumIT.Business.boost.upload.FirebaseStorageService;
 
 @SpringBootApplication
 public class BusinessBoostApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BusinessBoostApplication.class, args);
+		SpringApplication.run(BusinessBoostApplication.class, args);	
 	}
 	
 }
